fd1e4c51647d6a875e1953c45523479c92802078
wcf <wuchenfeng>
2020-10-13 00:47:56 +0800

    闭包学习

--- study/test08.go
+++ study/test08.go
@@ -5,12 +5,12 @@ import "fmt"
 //闭包
 func main() {
 	var zhang, huang int
-	task := excuteTask()
-	task2 := excuteTask()
-	huang = task("黄忠", 12) //将函数的值保存在该闭包中变量中国
-	zhang = task2("张飞", 14)
-	huang = task("黄忠", 5)
-	zhang = task2("张飞", 19)
+	task := excuteTask01("黄忠")
+	task2 := excuteTask01("张飞")
+	huang = task(12) //将函数的值保存在该闭包中变量中国
+	zhang = task2(14)
+	huang = task(5)
+	zhang = task2(19)
 	fmt.Println(huang)
 	fmt.Println(zhang)
 }
@@ -19,6 +19,18 @@ func main() {
 func excuteTask() func(name string, count int) (progress int) {
 	var progress = 0
 	doTask := func(name string, count int) int {
+		fmt.Println("%s行军打仗带领s%次", name, count)
+		progress += count
+		return progress
+	}
+	return doTask
+
+}
+
+//优化
+func excuteTask01(name string) func(count int) (progress int) {
+	var progress = 0
+	doTask := func(count int) int {
 		fmt.Println("%s行军打仗带领s%次", name, count)
 		progress += count
 		return progress
