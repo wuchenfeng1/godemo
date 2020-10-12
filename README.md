b7ecb82a93db320bfe667cfcb76df6079fe90383
wcf <wuchenfeng>
2020-10-12 09:31:19 +0800

    文档编写

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 [MD规范参考](https://www.cnblogs.com/bobo1/p/10929539.html)
 >目录结构：
  1. study  够浪~的演示demo
-     + 语法
+     + 基本数据类型
      + 流程控制语句
+     + iota  math
+     + 条件、循环、函数
  2. deep   深入探索目录
      + 手写中间件
      + 项目
--- study/test07.go
+++ study/test07.go
@@ -2,8 +2,18 @@ package main
 
 import "fmt"
 
+//函数的运用
 func main() {
-	superStart1("张学友", "刘德华", "黎明", "小明")
+	//superStart1("张学友", "刘德华", "黎明", "小明")
+	i := sum(4, 7)
+	he := sum2(3, 7)
+	h, cha, ji, shang := jishu(8, 2)
+	fmt.Println(i)
+	fmt.Println(he)
+	fmt.Println(h)
+	fmt.Println(cha)
+	fmt.Println(ji)
+	fmt.Println(shang)
 }
 
 //不定长的参数
@@ -18,4 +28,24 @@ func superStart(args ...string) {
 //固定参数个数
 func superStart1(arg1, arg2, arg3, arg4 string) {
 	fmt.Println(arg1, arg2, arg3, arg4)
+}
+
+//返回值
+func sum(a, b int) int {
+	return a + b
+}
+
+//自定义 返回名称返回
+func sum2(a, b int) (he int) {
+	he = a + b
+	return
+}
+
+// 多返回值
+func jishu(a, b int) (he, cha, ji, shang int) {
+	he = a + b
+	cha = a - b
+	ji = a * b
+	shang = a / b
+	return
 }
